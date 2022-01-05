700f0f9568532126ce11c162b39713a5fb1be94c
kishi4web <75372859+kishi4web@users.noreply.github.com>
2022-01-06 03:51:42 +0900

    Update main2.js

--- event/main2.js
+++ event/main2.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     let button = document.getElementById('xxx');
     button.addEventListener('click', function(){
         console.log('Thank you');
+        console.log('DOMContentLoaded を使った例');
     });
 });
 
